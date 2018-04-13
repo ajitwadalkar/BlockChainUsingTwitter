1776612a6cd2f08b60f933c228c69e49c0555486
ajitwadalkar <ajitwadalkar@gmail.com>
2018-04-13 11:52:51 -0400

    test file

--- src/main/java/edu/uc/crypt/test.java
+++ src/main/java/edu/uc/crypt/test.java
@@ -1,8 +1,14 @@
 package edu.uc.crypt;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Properties;
+
 public class test {
     public static void main(String[] args) {
-        TwitterInterface.initialize();
-        TwitterInterface.getTweets("master");
+       TwitterInterface.initialize();
+        TwitterInterface.getTweets("node02");
+
     }
 }
